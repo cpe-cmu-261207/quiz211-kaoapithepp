ad74f7c7eb2dbf9208d8a2c0e3ed3dbafdb8b295
Apithep P <49969602+kaoapithepp@users.noreply.github.com>
2021-02-02 13:57:58 +0700

    Nothing to say

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+function createData(name, gender, age) {
+  return { name, gender, age };
+}
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650
+  }
+});
+
 function App() {
   const persons = [
-    {
-      name: "Bob",
-      gender: "male",
-      age: "50"
-    },
-    {
-      name: "Alice",
-      gender: "male",
-      age: "20"
-    }
+    (createData = ("Bob", "male", 50)),
+    (createData = ("Alice", "male", 20)),
+    (createData = ("Apithep Piyaphiphathanamongkol", "male", 21))
   ];
+
+  const classes = useStyles();
   return (
     <div class="ml-2">
       <h3 class="title is-3">Person List</h3>
-
       {/* Convert me to a component! */}
-      <table class="table is-bordered mb-3">
+      {/* <table class="table is-bordered mb-3">
         <tbody>
           <tr>
             <th>Name</th>
@@ -29,7 +42,29 @@ function App() {
             <td>50</td>
           </tr>
         </tbody>
-      </table>
+      </table> */}
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="left">Name</TableCell>
+              <TableCell align="left">Gender</TableCell>
+              <TableCell align="left">Age</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {persons.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="center">{row.gender}</TableCell>
+                <TableCell align="center">{row.age}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
 
       {/* Code me please! */}
       <h3 class="title is-3">ID Counter</h3>
